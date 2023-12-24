088d8f00f042f4c447910a6fae22f61aa3a6ac29
Abhishek shukla <abhishekshukla824@gmail.com>
2023-12-24 19:14:04 +0530

    adding-about-code

--- main.go
+++ main.go
@@ -1,4 +1,9 @@
+/* This example will show how to create basic logging middleware in Go.
+ A middleware simply takes a http.HandlerFunc as one of its parameters, 
+ wraps it and returns a new http.HandlerFunc for the server to call. */
+
 package main
+
 
 import (
 	"fmt"
